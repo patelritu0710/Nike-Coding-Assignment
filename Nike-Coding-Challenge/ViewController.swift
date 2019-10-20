bb526cf5ad9347691e22273622677fc850684bda
renne.patel@united.com <renne.patel@united.com>
2019-10-20 18:18:47 -0500

    Repo created

---
+++ Nike-Coding-Challenge/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Nike-Coding-Challenge
+//
+//  Created by Ritu Patel on 10/20/19.
+//  Copyright © 2019 Ritu Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        buildScreen()
+    }
+
+    private func buildScreen() {
+        view.backgroundColor = .white
+    }
+}
+
